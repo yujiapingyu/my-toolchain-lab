38e08019f13f78b1a829c30dfdb93215890a895d
yujiaping <2507490921@qq.com>
2025-12-16 13:47:58 +0900

    增加内存泄漏

--- src/engine.c
+++ src/engine.c
@@ -1,8 +1,13 @@
 #include "engine.h"
+#include <stdlib.h>
 
 // 模拟一个简单的引擎控制逻辑
 // 如果温度 > 100，转速限制在 1000；否则转速 2000
 int calculate_rpm(int temperature) {
+
+    // 【静态检查能发现】: 申请了内存没释放 (内存泄露)
+    void *p = malloc(100);
+
     if (temperature > 100) {
         return 1000; // 过热保护
     } else if (temperature < -50) { // <--- 新增这个分支
