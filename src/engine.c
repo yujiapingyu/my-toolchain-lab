e5b98973e504725d883a157ba82abc8aa4035d3e
yujiaping <2507490921@qq.com>
2025-12-16 14:11:47 +0900

    update

--- src/engine.c
+++ src/engine.c
@@ -5,9 +5,6 @@
 // 如果温度 > 100，转速限制在 1000；否则转速 2000
 int calculate_rpm(int temperature) {
 
-    // 【静态检查能发现】: 申请了内存没释放 (内存泄露)
-    void *p = malloc(100);
-
     if (temperature > 100) {
         return 1000; // 过热保护
     } else if (temperature < -50) { // <--- 新增这个分支
